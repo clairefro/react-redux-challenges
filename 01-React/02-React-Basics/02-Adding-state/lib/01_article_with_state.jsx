e4cb62480e14edb2cea8b1a76f8404fcf6d88224
Germain Loret <germain.loret@gmail.com>
2018-01-08 16:07:25 +0100

    solved react challenge #2

--- 01-React/02-React-Basics/02-Adding-state/lib/01_article_with_state.jsx
+++ 01-React/02-React-Basics/02-Adding-state/lib/01_article_with_state.jsx
@@ -1,17 +1,43 @@
 // TODO: update dependencies if needed
-import React from 'react';
+import React, { Component } from 'react';
 
 // TODO: transform the following functional component into a class based component
 // TODO: set the initial state { clicked: false }
 // TODO: listen to the click event on `.article`
 // TODO: code the `handleClick` function (which adds a 'clicked' class to the `.article`)
-const Article = (props) => {
-  return (
-    <div className="article">
-      <h2 className="article-title">{props.title}</h2>
-      <p>{props.body}</p>
-    </div>
-  );
+// const Article = (props) => {
+//   return (
+//     <div className="article">
+//       <h2 className="article-title">{props.title}</h2>
+//       <p>{props.body}</p>
+//     </div>
+//   );
+// }
+
+class Article extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clicked: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    })
+  }
+
+  render() {
+    return (
+      <div className={this.state.clicked ? "article clicked" : "article"} onClick={this.handleClick}>
+        <h2 className="article-title">{this.props.title}</h2>
+        <p>{this.props.body}</p>
+      </div>
+    )
+  }
 }
 
 export default Article;
