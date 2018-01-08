37cb19f95742de2a9596a47c1016fc8f105e9099
Germain Loret <germain.loret@gmail.com>
2018-01-08 15:29:16 +0100

    solved react challenge #1

---
+++ 01-React/02-React-Basics/01-My-first-component/lib/01_article.jsx
@@ -0,0 +1,13 @@
+// TODO: import the right dependencies
+import React, {Component} from 'react'
+// TODO: code the Article component
+const Article = (props) => {
+  return (
+    <div className='article'>
+      <h2>{props.title}</h2>
+      <p>{props.body}</p>
+    </div>
+  )
+}
+// TODO: export your Article component for tests
+export default Article;
