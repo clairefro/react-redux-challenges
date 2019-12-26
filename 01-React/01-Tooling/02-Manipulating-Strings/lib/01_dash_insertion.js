572d64e6272e7455668b8b5b041571e220f5252e
claire froelich <claire.froelich@gmail.com>
2019-12-26 12:51:50 -0500

    completed challenge pt 1

--- 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
+++ 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
@@ -1,6 +1,9 @@
 function insertDash(word) {
   // TODO: implement the method and return word with dashes
-  return null;
+
+  const re = /([^aeiou\s])([^aeiou\s])/gi;
+  const result = word.replace(re, "$1-$2");
+  return result;
 }
 
 module.exports = insertDash;
