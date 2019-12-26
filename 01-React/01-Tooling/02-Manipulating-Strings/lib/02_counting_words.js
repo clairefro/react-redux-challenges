bba6f995322dcad271d12ce47ceef9ba39cc8b75
claire froelich <claire.froelich@gmail.com>
2019-12-26 13:24:06 -0500

    pt 2 complete

--- 01-React/01-Tooling/02-Manipulating-Strings/lib/02_counting_words.js
+++ 01-React/01-Tooling/02-Manipulating-Strings/lib/02_counting_words.js
@@ -1,5 +1,11 @@
 function occurrences(text, word) {
-  return null;
+  const pattern = `\\b${word}\\b`;
+  const re = new RegExp(pattern, 'gi');
+  const matches = text.match(re);
+  if (matches == null) {
+    return 0;
+  }
+  return matches.length;
 }
 
 module.exports = occurrences;
